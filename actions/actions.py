ce8001e8090184991b356e463c7197ea31c22059
mmillet88 <m.kosyuchenko@gmail.com>
2022-07-11 19:37:30 +0100

    geberate response

--- actions/actions.py
+++ actions/actions.py
@@ -39,6 +39,7 @@ from rasa_sdk.events import ActionExecuted
 from rasa_sdk.events import SessionStarted
 from rasa_sdk.events import EventType
 
+from actions.generateResponse import *
 
 import os
 import pandas as pd 
@@ -114,45 +115,12 @@ def generate_response_dataset(k=5, querry="Чувствую себя очень 
     top_10_similar = sorted_dataset['train'].select([0,1,2,3,4,5,6,7,8,9,10])
     return top_10_similar
 
-def generate_N_grams(text,ngram=1):
-  words=[word for word in text.split(" ")] 
-  # print("Sentence after removing stopwords:",words)
-  temp=zip(*[words[i:] for i in range(0,ngram)])
-  ans=[' '.join(ngram) for ngram in temp]
-  return ans    
-
-def generate_n_gram(sentence, max_n_gram):
-  n_gram_list = [generate_N_grams(sentence, n_gram) for n_gram in range(1, max_n_gram+1)]
-  return n_gram_list
-
-def novelty_score(sent_1_n_grams, sent_2_n_grams):
-  common_n_grams = [set(sent_1_n_grams[i]).intersection(set(sent_2_n_grams[i])) for i in range(len(sent_1_n_grams))]
-  score = [len(common_n_grams[i])/min(len(sent_1_n_grams[i]), len(sent_2_n_grams[i])) for i in range(len(common_n_grams))]
-  return sum(score)/len(score)
-
-def calculate_novelty(sentence,past_sentences):
-  new_sentence_n_grams = generate_n_gram(sentence, max_n_gram=3)
-  novelty = sum([novelty_score(new_sentence_n_grams, past_sentences[i]) for i in past_sentences])/len(past_sentences)
-  return novelty
-
-def score(row, prompt,past_sentences, novelty_weight=0.5, fluency_weight=0.5):
-  novelty_score = calculate_novelty(row[prompt], past_sentences=past_sentences)
-  fluency_score = row["ppl_"+prompt]
-  weighted_score = novelty_weight*novelty_score + fluency_weight*fluency_score
-  return {"score": weighted_score}
 
 def generate_next_response(prompt, past_sentences):
   dataset = load_from_disk('/Users/mariakosyuchenko/ChatBoth thesis/russian_chatbot/data/topKdataset')
   scores = dataset.map(lambda row: score(row, prompt, past_sentences))
   result = scores.sort('score')
   return result[-1][prompt]
-
-
-# class ResponsesSlot(Slot):
-#     def __init__(self):
-#         self.responses = {}
-#         self.n = 0
-
 
 
 class AskForSlotActionFeeling(Action):
@@ -163,9 +131,6 @@ class AskForSlotActionFeeling(Action):
         self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
     ) -> List[EventType]:
         dispatcher.utter_message(text="Please tell me how are you feeling now")
-        
-        dict = {}
-        dict[1] = ['hello']
         return []
 
 class AskForSlotActionEmotioPrediction(Action):
@@ -179,14 +144,6 @@ class AskForSlotActionEmotioPrediction(Action):
         current_feeling = tracker.get_slot("current_feeling")
         pred = example.predict_emotion(current_feeling)
         dispatcher.utter_message(f"I see your emotion as '{pred}'")
-        dataset = generate_response_dataset()
-        print(type(dataset))
-        sad_d = dataset.save_to_disk('/Users/mariakosyuchenko/ChatBoth thesis/russian_chatbot/data/topKdataset')
-
-        # if tracker.get_slot("emotion_confirmation")=='No':
-        #     print('success')
-        # else:
-        #     print("fail")
         return [SlotSet("emotion_prediction",pred),FollowupAction("action_emotion_confirmation")]
 
 class AskForSlotActionEmotionConfirmation(Action):
@@ -200,19 +157,9 @@ class AskForSlotActionEmotionConfirmation(Action):
         # test = tracker.get_slot("response")
         # print(test)
         emotion_prediction = tracker.get_slot("emotion_prediction")
-        # intent = tracker.get_intent_of_latest_message()
-        # print(intent)
-        # if intent == "affirm":
-        #     em_conf = "yes"
-        # else:
-        #     em_conf = "no"
-        # print(em_conf)
-        # buttons = [{"title": "Yes, this is accurate", "payload": '/affirm{"emotion_confirmation":"Yes"}'}, {"title": "No", "payload": '/deny{"emotion_confirmation":"No"}'}]
         buttons = [{"title": "Yes, this is accurate", "payload": '/correct_prediction{"emotion":"emotion_prediction"}'}, {"title": "No", "payload":'/not_correct_prediction'}]
         dispatcher.utter_message(text=f"Please confirm if your emotion is {emotion_prediction}, Did I understand your feeling correct? ", buttons=buttons)
-        # print(tracker.get_slot('emotion_confirmation'))
         return []
-        # return [SlotSet("emotion_confirmation",pred)]
 
 class AskForSlotActionEmotionConfirmation(Action):
     def name(self) -> Text:
@@ -223,6 +170,8 @@ class AskForSlotActionEmotionConfirmation(Action):
     ) -> List[EventType]:
         buttons = [{"title": "joy", "payload": '/is_recent{"emotion":"joy"}'}, {"title": "anger", "payload":'/is_recent{"emotion":"anger"}'}]
         dispatcher.utter_button_message(f"I am sorry, please select manually", buttons)
+        dataset = generate_response_dataset()
+        sad_d = dataset.save_to_disk('/Users/mariakosyuchenko/ChatBoth thesis/russian_chatbot/data/topKdataset')
         # print(tracker.get_slot('emotion_confirmation'))
         return []
 
@@ -235,14 +184,12 @@ class AskIfEventWasRecent(Action):
     ) -> List[EventType]:
         # buttons = [{"title": "Yes", "payload": '/is_recent{"emotion":"joy"}'}, {"title": "anger", "payload":'/is_recent{"emotion":"joy"}'}]
         # dispatcher.utter_button_message(f"I am sorry, please select manually", buttons)
-        dispatcher.utter_message(f"enough for today")
-        em = tracker.get_slot("emotion")
         buttons = [{"title": "yes", "payload": '/is_protocol_6_distressing'}, {"title": "no", "payload":'/is_protocol_11_distressing'}]
-        dispatcher.utter_button_message(f"Was this a recent event {em}", buttons)
-        first_message = dict()
-        first_message[1] = "Was this a recent event"
-        # print(tracker.get_slot('emotion_confirmation'))
-        return [SlotSet('pastResponses',first_message)]
+        text = generate_next_response(prompt="how_recent_is_the_event?", past_sentences=tracker.get_slot('pastResponses'))
+        print(text)
+        updated_responses = add_response_to_past_responses(latest_response=text, past_responses=tracker.get_slot('pastResponses'))
+        dispatcher.utter_message(text=text, buttons=buttons)
+        return [SlotSet('pastResponses',updated_responses)]
 
 class Action6distressing(Action):
     def name(self) -> Text:
@@ -251,15 +198,12 @@ class Action6distressing(Action):
     def run(
         self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
     ) -> List[EventType]:
-        prot = [13, 17]
-        print(tracker.get_slot('pastResponses'))
-        past_responses = tracker.get_slot('pastResponses')
-        test = generate_next_response(prompt="how_recent_is_the_event?", past_sentences=past_responses)
-        print(test)
-        past_responses[len(past_responses)+1] = test
+        text = generate_next_response(prompt="method_6", past_sentences=tracker.get_slot('pastResponses'))
+        print(text)
+        updated_responses = add_response_to_past_responses(latest_response=text, past_responses=tracker.get_slot('pastResponses'))
         buttons = [{"title": "yes", "payload": '/ok_to_ask_more{"protocols_1":["13","25"]}'}, {"title": "no", "payload":'/ok_to_ask_more{"protocols_1":[6]}'}]
-        dispatcher.utter_button_message(f"Did you find protocol 6 distressing?", buttons)
-        return [SlotSet('pastResponses',past_responses)]
+        dispatcher.utter_message(text=text, buttons=buttons)
+        return [SlotSet('pastResponses',updated_responses)]
 
 class AskMoreQuestions(Action):
     def name(self) -> Text:
@@ -272,15 +216,17 @@ class AskMoreQuestions(Action):
         # dispatcher.utter_button_message(f"I am sorry, please select manually", buttons)
         protocols_so_far = tracker.get_slot("protocols_1")
         em = tracker.get_slot("emotion")
-        dispatcher.utter_message(f"enough for today{protocols_so_far} and {type(protocols_so_far)} so far and {em}")
+        text = generate_next_response(prompt="additional_questions_permission", past_sentences=tracker.get_slot('pastResponses'))
+        print(text)
+        updated_responses = add_response_to_past_responses(latest_response=text, past_responses=tracker.get_slot('pastResponses'))
         buttons = [{"title": "yes", "payload": '/strong_emotions'}, {"title": "no", "payload":'/strong_emiotions'}]
-        dispatcher.utter_button_message(f"is it ok to ask more questions", buttons)
+        dispatcher.utter_message(text=text, buttons=buttons)
         past_responses = tracker.get_slot('pastResponses')
         print(past_responses)
         # buttons = [{"title": "yes", "payload": '/ok_to_ask_more'}, {"title": "no", "payload":'/ok_to_ask_more'}]
         # dispatcher.utter_button_message(f"Was this a recent event", buttons)
         # print(tracker.get_slot('emotion_confirmation'))
-        return []
+        return [SlotSet('pastResponses',updated_responses)]
 
 class AdditionalQuestionsStrongEmotion(Action):
     def name(self) -> Text:
@@ -294,7 +240,9 @@ class AdditionalQuestionsStrongEmotion(Action):
         protocols_so_far = tracker.get_slot("protocols_1")
         print(f"protocols {protocols_so_far} and {type(protocols_so_far)}")
         em = tracker.get_slot("emotion")
-        message = "Have you strongly expressed following emotions?"
+        text = generate_next_response(prompt="ppl_feeling_envy_mistrust_trigger", past_sentences=tracker.get_slot('pastResponses'))
+        print(text)
+        updated_responses = add_response_to_past_responses(latest_response=text, past_responses=tracker.get_slot('pastResponses'))
         # new_protocols = protocols_so_far.copy()
         # new_protocols.append(25)
         # print(type(new_protocols))
@@ -302,11 +250,11 @@ class AdditionalQuestionsStrongEmotion(Action):
         new_protocols = 7
 
         buttons = [{"title": "yes", "payload": '/recommend_protocols{"protocols_2":[13,14]}'}, {"title": "no", "payload":'/ok_to_ask_more'}]
-        dispatcher.utter_button_message(message, buttons)
+        dispatcher.utter_message(text=text, buttons=buttons)
         # buttons = [{"title": "yes", "payload": '/ok_to_ask_more'}, {"title": "no", "payload":'/ok_to_ask_more'}]
         # dispatcher.utter_button_message(f"Was this a recent event", buttons)
         # print(tracker.get_slot('emotion_confirmation'))
-        return []
+        return [SlotSet('pastResponses',updated_responses)]
 
 
 
---
+++ actions/generateResponse.py
@@ -0,0 +1,51 @@
+def generate_N_grams(text,ngram=1):
+  words=[word for word in text.split(" ")] 
+  # print("Sentence after removing stopwords:",words)
+  temp=zip(*[words[i:] for i in range(0,ngram)])
+  ans=[' '.join(ngram) for ngram in temp]
+  return ans    
+
+def generate_n_gram(sentence, max_n_gram):
+  n_gram_list = [generate_N_grams(sentence, n_gram) for n_gram in range(1, max_n_gram+1)]
+  return n_gram_list
+
+def novelty_score(sent_1_n_grams, sent_2_n_grams):
+#   print(sent_1_n_grams, sent_2_n_grams)
+  common_n_grams = [set(sent_1_n_grams[i]).intersection(set(sent_2_n_grams[i])) for i in range(len(sent_1_n_grams))]
+  try:
+    score = [len(common_n_grams[i])/min(len(sent_1_n_grams[i]), len(sent_2_n_grams[i])) for i in range(len(common_n_grams))]
+    score =sum(score)/len(score)
+  except ZeroDivisionError:
+    score = 0
+
+  return score
+
+def calculate_novelty(sentence,past_sentences):
+  new_sentence_n_grams = generate_n_gram(sentence, max_n_gram=3)
+  print(len(past_sentences))
+  novelty = sum([novelty_score(new_sentence_n_grams, past_sentences[str(i)]) for i in range(len(past_sentences))])/len(past_sentences)
+  return novelty
+
+def score(row, prompt,past_sentences, novelty_weight=0.5, fluency_weight=0.5):
+  if past_sentences is None:
+    novelty_score = 0
+  else:
+    if row[prompt] != "" and isinstance(row[prompt], str):
+        novelty_score = calculate_novelty(row[prompt], past_sentences=past_sentences)
+    else:
+        novelty_score = 0 
+  if type(row[prompt])!=str or len(row[prompt]) < 3:
+    fluency_score = 0
+  else:
+    fluency_score = row["ppl_"+prompt]
+  weighted_score = novelty_weight*novelty_score + fluency_weight*fluency_score
+  return {"score": weighted_score}
+
+def add_response_to_past_responses(latest_response, past_responses):
+    latest_response_n_grams = generate_n_gram(latest_response, max_n_gram=3)
+    if past_responses is None:
+        past_responses = dict()
+        past_responses[0] = latest_response_n_grams
+    else:
+        past_responses[len(past_responses)] = latest_response_n_grams
+    return past_responses
