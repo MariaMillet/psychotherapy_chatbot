e9515a399879664c24a0532907c8f4b5a5bc24e6
mmillet88 <m.kosyuchenko@gmail.com>
2022-08-03 12:12:33 +0100

    add import file

---
+++ import_data.py
@@ -0,0 +1,18 @@
+import os
+path = "data/respGenDataset"
+os.chdir(path)
+import gdown
+
+id = "1-HLgylzSs2TfRgzYLbhKBe1nRojJcVJF"
+gdown.download(id=id)
+
+path = "train"
+os.chdir(path)
+
+id = "1-JVZQrBTxt3_OSSeT4npf-HI2rzMUnIt" 
+gdown.download(id=id)
+
+id = "1-k_Si6MHqqJNhYJSbneRtvTxP24heD_7"
+gdown.download(id=id)
+id ="1-SHcbjqYHhQOAVagzRuZUzLLVtLecjy5"
+gdown.download(id=id)
