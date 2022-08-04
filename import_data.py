2a4857d77ca2d5c574f83103c4f7361d57eafb11
Maria <gregmaria@MacBook-Pro-de-Gregoire.local>
2022-08-04 21:20:32 +0100

    addded import files

--- import_data.py
+++ import_data.py
@@ -16,3 +16,25 @@ id = "1-k_Si6MHqqJNhYJSbneRtvTxP24heD_7"
 gdown.download(id=id)
 id ="1-SHcbjqYHhQOAVagzRuZUzLLVtLecjy5"
 gdown.download(id=id)
+
+os.chdir('../')
+print(os.getcwd())
+os.chdir('../')
+print(os.getcwd())
+
+path = "../data/respSyntheticDataset"
+os.chdir(path)
+import gdown
+
+# https://drive.google.com/file/d/11ENN2dq2QKEM7SUFS7egQAf0Z8N315kp/view?usp=sharing
+# https://drive.google.com/file/d/11HjDBs9GhIHp5aR5SUPO2Kn2J9RGXI1U/view?usp=sharing
+# https://drive.google.com/file/d/11F3XiDdawUCaWznLo2XTfdlo1QAZ98jf/view?usp=sharing
+id = "11ENN2dq2QKEM7SUFS7egQAf0Z8N315kp"
+gdown.download(id=id)
+
+
+id = "11HjDBs9GhIHp5aR5SUPO2Kn2J9RGXI1U" 
+gdown.download(id=id)
+
+id = "11F3XiDdawUCaWznLo2XTfdlo1QAZ98jf"
+gdown.download(id=id)
